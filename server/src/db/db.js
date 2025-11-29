dd11185b2175f45ba21839a15f78ed2e98190b13
Tarapada Garai <146563644+codingWithRakesh@users.noreply.github.com>
2025-11-30 00:35:14 +0530

    feat: Add room index synchronization during database connection

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js";
+import { Room } from "../models/room.model.js";
 
 const connectDB = async () => {
     try {
         const connectionIn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log("mongoDB connected",connectionIn.connection.host)
+
+        await Room.syncIndexes();
+        console.log("Room indexes synced!");
     } catch (error) {
         console.log("MongoDB Failed",error?.message)
         process.exit(1)
